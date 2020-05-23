d8f28d7277c5089ca38ecde7546efdf0cb771b5a
Wenqi He <hewenqi96@gmail.com>
2020-05-23 07:18:48 -0400

    Update

--- src/components/DetailToolbar.js
+++ src/components/DetailToolbar.js
@@ -3,12 +3,17 @@ import IconButton from "./IconButton";
 const DetailToolbar = ({ canDelete, deleteMail }) => {
   return (
     // use-transform
+    // prettier-ignore
     [
       IconButton(
         (type = "arrow-left"),
         (onclick = () => window.history.back())
       ),
-      canDelete && IconButton((type = "trash"), (onclick = deleteMail)),
+      canDelete &&
+        IconButton(
+          (type = "trash"),
+          (onclick = deleteMail)
+        ),
     ]
   );
 };
