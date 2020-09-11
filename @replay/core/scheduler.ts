08057682e838d6847116bf055a34370de84c898d
Wenqi He <hewenqi96@gmail.com>
2020-09-11 14:56:15 +0800

    Refactor

--- @replay/core/scheduler.ts
+++ @replay/core/scheduler.ts
@@ -237,19 +237,3 @@ export class Scheduler {
     this.pendingUpdates.delete(entry);
   }
 }
-
-export function render(elements: Quasiquote[], container: HTMLElement): void {
-  // Hack: Rendering `elements` under `container` is equivalent to rendering `[container, null, elements]`
-  // Hack: Simulate an imaginary previous render of `[container, null, null]`
-  const type = container.tagName.toLowerCase();
-  const entry = new ActivationRecord(type, null);
-  entry.node = container;
-  container.innerHTML = "";
-  container.append(new Text());
-
-  // Hack: Use memoized props to simulate passing in props as arguments,
-  // and use dirty bit to trigger re-render
-  entry.props = { children: elements };
-  entry.dirty = true;
-  Scheduler.instance.requestUpdate(new Set([entry]));
-}
