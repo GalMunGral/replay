8258fc16535656ea55c7fd85b68da0deb70a2fb8
Wenqi He <hewenqi96@gmail.com>
2020-10-01 09:04:26 +0800

    update

--- modulizer/transforms/es-module.js
+++ modulizer/transforms/es-module.js
@@ -14,10 +14,7 @@ module.exports = (file) => {
     const filePath = resource.startsWith(".") // TODO: not sure about this
       ? resolve(path.join(context, resource)) // relative import
       : resolve(path.join(root, "node_modules", resource)); // node module
-
-    // Collect dependencies for HTTP/2 server push
-    deps.add(filePath);
-
+    deps.add(filePath); // -> this is for h2 server push
     return "/" + path.relative(root, filePath);
   }
 
@@ -25,9 +22,11 @@ module.exports = (file) => {
     sourceType: "module",
     plugins: ["jsx", "classProperties"],
   });
-
   traverse(ast, {
     ImportDeclaration({ node }) {
+      node.source.value = resolveDep(node.source.value);
+    },
+    ExportAllDeclaration({ node }) {
       node.source.value = resolveDep(node.source.value);
     },
     ExportNamedDeclaration({ node }) {
@@ -36,10 +35,12 @@ module.exports = (file) => {
         node.source.value = resolveDep(node.source.value);
       }
     },
-    ExportAllDeclaration({ node }) {
-      node.source.value = resolveDep(node.source.value);
-    },
   });
+  const result = generate(ast);
 
-  return { ...file, content: generate(ast).code, deps };
+  return {
+    ...file,
+    content: result.code,
+    deps,
+  };
 };
